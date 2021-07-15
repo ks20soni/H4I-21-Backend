f658262f4f4cf5bb45a8d3301ca6b49fc9b991a1
khushi soni <khushisoni@khushis-MacBook-Pro.local>
2021-07-16 03:18:37 +0530

    .env added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,150 +1,163 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const client = require("../configs/db");
-
-exports.signup = (req, res) => {
-  //taking name, email and password in body everytime a user signs up
-  const { name, email, password } = req.body;
-
+const client = require("../configurations/db");
+exports.signUp = (req, res) => {
+  const { name, email, password, contact } = req.body;
   client
-    .query(`SELECT * FROM users where email = '${email}';`)
+    .query(`SELECT * FROM users where email='${email}';`)
     .then((data) => {
-      isValid = data.rows;
-
-      //if length of the array is not empty then the user already exists
-      if (isValid.length != 0) {
+      const exists = data.rows;
+      if (exists.length !== 0) {
         res.status(400).json({
-          error: "User already exists",
+          error: "User alerady exists",
         });
       } else {
-        //if the user does not exist we are hashing the plain password entered by the user
         bcrypt.hash(password, 10, (err, hash) => {
           if (err) {
-            res.status(400).json({
-              error: "Internal server error",
+            res.status(500).json({
+              message: "Internal server error",
             });
           }
-
-          //the array user has name, email and password
           const user = {
             name,
             email,
             password: hash,
+            contact,
           };
-          console.log(user);
-
-          //inserting name, email and password(hashed) in database
+          const token = jwt.sign(
+            {
+              email: email,
+            },
+            process.env.SECRET_KEY,
+            {
+              expiresIn:'12h'
+            }
+          );
           client
             .query(
-              `INSERT INTO users (name, email, password) VALUES ('${user.name}', '${user.email}', '${user.password}');`
+              `INSERT INTO users (name , email, password ,contact) VALUES('${user.name}','${user.email}','${user.password}','${user.contact}' );`
             )
-
-            //if the user is signed up to the databse a token is generated for the user
             .then((data) => {
-              const token = jwt.sign(
-                {
-                  email: email,
-                },
-                process.env.SECRET_KEY
-              );
-
-              // a success message and a token is sent back to the user as response
               res.status(200).json({
-                message: "User added successfully",
+                message: "User added succesfully!!!!",
                 token: token,
               });
             })
-
-            //if an error occurs while saving the data to databse this response is sent
             .catch((err) => {
+              console.log("hi");
               res.status(500).json({
-                error: "Database error occurred",
+                error: "Servor error try after sometime",
               });
             });
-        });
-      }
-    })
-
-    //if an error occus while checking for the user this error is sent
-    .catch((err) => {
-      res.status(500).json({
-        error: "Database error occurred",
+          });
+        }
+      })
+      .catch((err) => {
+        res.status(500).json({
+        error: "Servor error try after sometime",
       });
     });
 };
 
-exports.signin = (req, res) => {
-  //taking email and password in body everytime a user signs in
-  const { email, password } = req.body;
+exports.signIn=(req,res)=>{
+  const {email, password}=req.body;
+client.query(`SELECT * FROM users where email='${email}'`)
+.then((data)=>{
+  const exists=data.rows;
+  if(exists.length===0){
+    res.status(400).json(
+      {
+        error:"User does not exist",
+      }
+    );
+}
+else{
+  bcrypt.compare(password,exists[0].password,(err,result)=>{
+if(err){
+  res.status(400).json({
+    error:"Server down try later!",
+  });
+}
+else if(result===true){
+  const token=jwt.sign(
+  {
+    email:email,
+  },
+  process.env.SECRET_KEY,
+  {
+    expiresIn:'12h'
+  }
+  );
+res.status(200).json({
+  message:"Signed In Successfully",
+  token:token
+});
+}
+else{
+  res.status(400).json({
+    error:"Incorrect password",
+  });
+}
+  })
+}
 
-  //selecting all values from users table with a particular email
-  client
-    .query(`SELECT * FROM users where email = '${email}';`)
-    .then((data) => {
-      userData = data.rows; //assigning the data of a particular email to userData array
+})
+.catch((err)=>{
+res.status(500).json({
+  error:"Internal server error",
+})
+});
+}
 
-      //checking if the user already exists in database or not
-      if (userData.length == 0) {
+exports.changePassword=(req,res)=>{
+const{oldPass,newPass,email}=req.body;
+client.query(`SELECT password FROM users where email='${email}'`)
+.then((database_res)=>{
+  const Pass=database_res.rows[0].password;
+  // console.log(Pass)
+  if(oldPass===newPass){
+    res.status(400).json({
+      message:"Please Enter A different Password"
+    })
+  }
+  else{
+    bcrypt.compare(oldPass,Pass,(err,result)=>{
+      if(err){
         res.status(400).json({
-          error: "User does not exist, sign up instead",
-        });
-      } else {
-        //if user already exists in the database we will need to check the password entered by user and password stored in database
-        bcrypt.compare(password, userData[0].password, (err, result) => {
-          if (err) {
-            //while comparing the passwords if an error occurs this response is sent
-            res.status(500).json({
-              error: "Server error",
-            });
-          }
-          //if the passwords match that is, the password stored in database and the password entered by user we sign token for the user
-          else if (result == true) {
-            const token = jwt.sign(
-              {
-                email: email,
-              },
-              process.env.SECRET_KEY
-            );
-            //if the user is signed in successfully we display a resposne and a token
-            res.status(200).json({
-              message: "User signed in successfully",
-              token: token,
-            });
-          } else {
-            //if the password is not correct this response is sent
-            res.status(400).json({
-              error: "Enter correct password",
-            });
-          }
+          message:"Server down try later!",
         });
       }
-    })
-    //if there is an error while finding the user in databse an error message is sent
-    .catch((err) => {
-      res.status(500).json({
-        error: "Database error occurred",
-      });
-    });
-};
+      else if(result===true){
+        bcrypt.hash(newPass, 10, (err, hash) => {
+          if (err) {
+            res.status(500).json({
+              message: "Internal server error",
+            });
+          }
+          else{
+            client.query(`UPDATE users SET password = '${hash}' WHERE email = '${email}'`).then(() => {
+              res.status(200).json({
+                message:"Password Changed Successfully!"
+              });
+            }).catch((error) => {
+              res.status(500).json({
+                message:"Database Error :)"
+              });
+            })
+          }
+        })
 
-//getuser route
-exports.getuser = (req, res) => {
-  client
-    .query(`SELECT * from users where email = '${req.email}'`)
-    .then((data) => {
-      console.log(data);
-      const { name, email, id } = data.rows[0];
-      //sending name email and password as response
-      res.send({
-        name,
-        email,
-        id,
-      });
-    })
-    //if an error occurs while fetchung the data this response is sent
-    .catch((err) => {
-      res.status(500).json({
-        message: "Database error",
-      });
+      }
+      else{
+        res.status(400).json({
+          message:"Incorrect Old password"
+        });
+      }
+        })
+  }
+  }).catch((error) => {
+    res.status(500).json({
+      message:"Database Error :)"
     });
-};
+  })
+}
--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,32 +1,21 @@
-const jwt = require("jsonwebtoken");
-const client = require("../configs/db");
+const jwt = require('jsonwebtoken');
+const client = require('../configurations/db');
 
-exports.verifyToken = (req, res, next) => {
-  const token = req.headers.authorization;
-  jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
-    if (err) {
-      res.status(500).json({ error: "Server error" });
-    }
-    const userEmail = decoded.email;
-
-    client
-      .query(`SELECT * FROM users WHERE email = '${userEmail}';`)
-      .then((data) => {
-        if (data.rows.length === 0) {
-          res.status(400).json({
-            message: "Verification failed",
-          });
-        } else {
-          console.log(data.rows);
-          req.email = userEmail;
-          req.name = data.rows[0].name;
-          next();
+exports.checkToken = (req, res, next) => {
+    const token = req.headers.authorization;
+    jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+        if (err) {
+            res.status(400).json({
+                error:"Invalid Token"
+            });
         }
-      })
-      .catch((err) => {
-        res.status(500).json({
-          message: "Database error",
-        });
-      });
-  });
-};
+
+        else {
+            const email = decoded.email;
+            req.body.email = email;
+            next();
+        }
+
+    })
+
+}
